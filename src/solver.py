5a664671095783a35ffc0bc402370569b5c98ffa
khang200923 <92591231+khang200923@users.noreply.github.com>
2025-05-10 00:14:04 +0700

    use cdcl

--- src/solver.py
+++ src/solver.py
@@ -1,55 +1,60 @@
 from dataclasses import dataclass, field
-from typing import Dict, List, Tuple
+from typing import Dict, List, Set, Tuple
+import random
+
+Literal = Tuple[str, bool]
+LiteralInt = Tuple[str, bool, int]
+Clause = Tuple[Literal, Literal, Literal]
+ExtendedClause = List[Literal]
 
 @dataclass
 class Solver:
-    clauses: List[Tuple[Tuple[int, bool], Tuple[int, bool], Tuple[int, bool]]] = field(default_factory=list)
-    num_vars: int = 0
-    num_clauses: int = 0
-    assignments: Dict[int, bool] = field(init=False)
+    clauses: List[Clause] = field(default_factory=list)
+    unsolved_clauses: Set[int] = field(default_factory=set)
+    assignments: Dict[Literal, int] = field(init=False)
+    unassigned_variables: Set[str] = field(init=False)
+    reasoning: Dict[LiteralInt, List[LiteralInt]] = field(init=False)
+    current_decision_level: int = field(init=False)
 
     def __post_init__(self):
+        self.unassigned_variables = set(literal[0] for clause in self.clauses for literal in clause)
         self.assignments = dict()
+        self.reasoning = dict()
+        self.current_decision_level = 0
 
-    def add_clause(self, clause: Tuple[Tuple[int, bool], Tuple[int, bool], Tuple[int, bool]]):
+    def add_clause(self, clause: Clause):
         self.clauses.append(clause)
-        self.num_clauses += 1
-        for literal in clause:
-            var, _ = literal
-            self.num_vars = max(self.num_vars, var + 1)
+        self.unsolved_clauses.add(len(self.clauses) - 1)
+        self.unassigned_variables.update(literal[0] for literal in clause)
 
-    def solve(self):
-        self.assignments = dict()
-        self.backtrack(0)
+    def unit_propagation(self) -> bool:
+        progress = False
+        for i in self.unsolved_clauses.copy():
+            clause = self.clauses[i]
+            leftovers = [literal for literal in clause if (literal[0], not literal[1]) not in self.assignments]
+            falsifiers = [(literal[0], not literal[1], self.assignments[(literal[0], not literal[1])]) for literal in clause if (literal[0], not literal[1]) in self.assignments]
+            assert len(leftovers) > 0
+            if len(leftovers) == 1:
+                self.assign(leftovers[0], falsifiers)
+                self.unsolved_clauses.remove(i)
+                progress = True
+        return progress
 
-    def backtrack(self, level: int):
-        if level > self.num_vars:
-            return
+    def assign(self, literal: Literal, reasoning: List[LiteralInt]):
+        assert literal not in self.reasoning
+        self.assignments[literal] = self.current_decision_level
+        self.reasoning[literal + (self.current_decision_level,)] = reasoning
+        self.unassigned_variables.discard(literal[0])
 
-        self.assignments[level] = True
-        if self.is_satisfied():
-            self.backtrack(level + 1)
-            if level + 1 not in self.assignments:
-                return
+    def decision_heuristic(self) -> Literal:
+        # just use a random literal whatever
+        return (random.sample(self.unassigned_variables, 1)[0], random.choice((True, False)))
 
-        self.assignments[level] = False
-        if self.is_satisfied():
-            self.backtrack(level + 1)
-            if level + 1 not in self.assignments:
-                return
+    def conflict(self) -> str | None:
+        conflicty = [literal[0] for literal in self.assignments if (literal[0], not literal[1]) in self.assignments]
+        assert len(conflicty) <= 1
+        if conflicty:
+            return conflicty[0]
+        return
 
-        del self.assignments[level]
-
-    def is_satisfied(self) -> bool:
-        for clause in self.clauses:
-            if not any(self.evaluate_literal(literal) for literal in clause):
-                return False
-        return True
-
-    def evaluate_literal(self, literal: Tuple[int, bool]) -> bool:
-        var, negated = literal
-        if var not in self.assignments:
-            return True
-        if negated:
-            return self.assignments[var] is False
-        return self.assignments[var] is True
+    # W.I.P
