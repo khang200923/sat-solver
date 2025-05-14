c52abea0d5541617c59ad574467ff81bf0e81f91
khang200923 <92591231+khang200923@users.noreply.github.com>
2025-05-14 23:17:06 +0700

    add untested functions and remove premature optimizations

--- src/solver/solver.py
+++ src/solver/solver.py
@@ -7,38 +7,35 @@ from solver.utils import li, lii, cl, ec
 @dataclass
 class Solver:
     clauses: List[Clause] = field(default_factory=list)
-    unsolved_clauses: Set[int] = field(default_factory=set)
     assignments: Dict[Literal, int] = field(init=False)
-    unassigned_variables: Set[str] = field(init=False)
     reasoning: Dict[LiteralInt, List[LiteralInt]] = field(init=False)
     current_decision_level: int = field(init=False)
+    variables: Set[str] = field(init=False)
 
     def __post_init__(self):
-        self.unassigned_variables = set(literal[0] for clause in self.clauses for literal in clause)
         self.assignments = dict()
         self.reasoning = dict()
         self.current_decision_level = 0
+        self.variables = set(literal[0] for clause in self.clauses for literal in clause)
         self.add_clause(cl("true true true"))
 
     def add_clause(self, clause: Clause):
         self.clauses.append(clause)
-        self.unsolved_clauses.add(len(self.clauses) - 1)
-        self.unassigned_variables.update(literal[0] for literal in clause)
+        self.variables.update(literal[0] for literal in clause)
 
     def unit_propagation(self) -> bool:
         progress = False
-        for i in self.unsolved_clauses.copy():
-            clause = self.clauses[i]
+        for clause in self.clauses:
+            if any(literal in self.assignments for literal in clause):
+                continue
             leftovers = set(literal for literal in clause if (literal[0], not literal[1]) not in self.assignments)
             falsifiers = set((literal[0], not literal[1], self.assignments[(literal[0], not literal[1])]) for literal in clause if (literal[0], not literal[1]) in self.assignments)
             if len(leftovers) == 0:
                 # conflict
                 self.assign(li('!true'), list(falsifiers))
-                self.unsolved_clauses.remove(i)
                 return True
             if len(leftovers) == 1:
                 self.assign(list(leftovers)[0], list(falsifiers))
-                self.unsolved_clauses.remove(i)
                 progress = True
         return progress
 
@@ -46,11 +43,13 @@ class Solver:
         assert literal not in self.reasoning
         self.assignments[literal] = self.current_decision_level
         self.reasoning[literal + (self.current_decision_level,)] = reasoning
-        self.unassigned_variables.discard(literal[0])
+
+    def unassigned_variables(self) -> Set[str]:
+        return {variable for variable in self.variables if li(variable) not in self.assignments and li('!' + variable) not in self.assignments}
 
     def decision_heuristic(self) -> Literal:
         # just use a random literal whatever
-        return (random.sample(self.unassigned_variables, 1)[0], random.choice((True, False)))
+        return (random.sample(self.unassigned_variables(), 1)[0], random.choice((True, False)))
 
     def conflict(self) -> bool:
         return li('!true') in self.assignments
@@ -83,8 +82,22 @@ class Solver:
                     lowers.add(causes[:2])
         return list((literal[0], not literal[1]) for literal in lowers | leftovers)
 
+    def backjump_level_heuristic(self, conflict_clause: ExtendedClause) -> int:
+        return max((self.assignments[literal] for literal in conflict_clause if self.assignments[literal] < self.current_decision_level), default=0)
+
     def decide(self, literal: Literal):
         if literal in self.assignments:
             raise ValueError(f"Literal {literal} already assigned")
         self.current_decision_level += 1
         self.assign(literal, [])
+
+    def backjump(self, level: int):
+        if level >= self.current_decision_level:
+            raise ValueError(f"Level {level} is not less than current decision level {self.current_decision_level}")
+        self.current_decision_level = level
+        for literal in list(self.assignments.keys()):
+            if self.assignments[literal] > level:
+                del self.assignments[literal]
+        for literal in list(self.reasoning.keys()):
+            if literal[2] > level:
+                del self.reasoning[literal]
